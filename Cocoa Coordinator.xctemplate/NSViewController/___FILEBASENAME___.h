622da0f4ec069ce961b2e90ef0e2d0210694656a
Frank Gregor <phranck@cocoanaut.com>
2016-04-29 02:28:08 +0200

    updated template header files

--- Cocoa Coordinator.xctemplate/NSViewController/___FILEBASENAME___.h
+++ Cocoa Coordinator.xctemplate/NSViewController/___FILEBASENAME___.h
@@ -53,7 +53,7 @@
 #pragma mark - Coordinator Properties
 @property (nonatomic, strong, readonly) __kindof ___VARIABLE_navigationControllerClass___ *navigationController;
 @property (nonatomic, strong) NSMutableArray *childCoordinators;
-@property (weak) id<___FILEBASENAMEASIDENTIFIER___Delegate> delegate;
+@property (weak, readonly) id<___FILEBASENAMEASIDENTIFIER___Delegate> delegate;
 
 
 #pragma mark - API
