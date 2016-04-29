0c44fe8a7e5ce6c6071e0913db6c420a2683591e
Frank Gregor <phranck@cocoanaut.com>
2016-04-29 02:07:07 +0200

    • updated screenshot • restructured templates and naming to refine separation from iOS to OS X

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 CLONE_DIR=$(pwd)
 TEMPLATE_DIR="Library/Developer/Xcode/Templates/File Templates/Source/"
 DEST_DIR="$HOME/$TEMPLATE_DIR"
-TEMPLATE='Objective-C Coordinator.xctemplate'
+iOS_TEMPLATE='Cocoa Touch Coordinator.xctemplate'
+OSX_TEMPLATE='Cocoa Coordinator.xctemplate'
 
 cd "$HOME"
 mkdir -p "$TEMPLATE_DIR"
-cp -a "$CLONE_DIR/$TEMPLATE" "$DEST_DIR"
+cp -a "$CLONE_DIR/$iOS_TEMPLATE" "$DEST_DIR"
+cp -a "$CLONE_DIR/$OSX_TEMPLATE" "$DEST_DIR"
