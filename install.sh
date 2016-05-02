1bcdec935bbf47145f3ab0957ddecea0545bb882
Frank Gregor <phranck@cocoanaut.com>
2016-05-02 16:46:30 +0200

    new install script

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-CLONE_DIR=$(pwd)
-TEMPLATE_DIR="Library/Developer/Xcode/Templates/File Templates/Source/"
-DEST_DIR="$HOME/$TEMPLATE_DIR"
+VERSSION="1.0.0"
+CURRENT_DIR=$(pwd)
+TEMP_DIR="/tmp"
+ARCHIVE_NAME="Xcode-Coordinator-Template"
+RELEASE_URL="https://github.com/phranck/Xcode-Coordinator-Template/archive/$VERSSION.zip"
+UNZIP_DIR="$TEMP_DIR/$ARCHIVE_NAME-$VERSSION"
+TEMPLATE_DIR="$HOME/Library/Developer/Xcode/Templates/File Templates/Source/"
 iOS_TEMPLATE='Cocoa Touch Coordinator.xctemplate'
 OSX_TEMPLATE='Cocoa Coordinator.xctemplate'
 
-cd "$HOME"
-mkdir -p "$TEMPLATE_DIR"
-cp -a "$CLONE_DIR/$iOS_TEMPLATE" "$DEST_DIR"
-cp -a "$CLONE_DIR/$OSX_TEMPLATE" "$DEST_DIR"
+cd "$TEMP_DIR"
+curl -SL $RELEASE_URL > "$ARCHIVE_NAME.zip"
+unzip -oq "$ARCHIVE_NAME.zip"
+
+cp -a "$UNZIP_DIR/$iOS_TEMPLATE" "$TEMPLATE_DIR"
+cp -a "$UNZIP_DIR/$OSX_TEMPLATE" "$TEMPLATE_DIR"
+
+rm -rf "$ARCHIVE_NAME.zip"
+rm -rf "$UNZIP_DIR"
+
+cd "$CURRENT_DIR"
+echo "\n😊  $ARCHIVE_NAME successfuly installed.\n"
